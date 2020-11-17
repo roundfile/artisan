208b4d00b53163602eaebfb9b9b9fe28583ca8da
Dave <1648748+roundfile@users.noreply.github.com>
2020-11-17 08:55:21 -0800

    ping3

--- src/build-mac3.sh
+++ src/build-mac3.sh
@@ -4,13 +4,10 @@
 set -e
 
 # add argument "legacy" to make a build that supports older OS X systems using an outdated Qt
-echo "Next is appveyor"
-printf $APPVEYOR
-echo "prev was appveyor"
 
 if [ ! -z $APPVEYOR ]; then
     # Travis CI builds
-    printf "Appveyor build"
+    echo "NOTICE: Appveyor build"
     export PYTHON=/usr/local
     export PYTHONBIN=$PYTHON/bin
     export PYTHONPATH=$PYTHON/lib/python3.8
@@ -21,6 +18,7 @@ if [ ! -z $APPVEYOR ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.15
     export ARTISAN_LEGACY_BUILD=false
 elif [[ "$1" = "legacy" ]]; then
+    echo "NOTICE: legacy build"
     # local legacy build featuring an outdated Qt to minimize the DEPLOYMENT_TARGET supporting older system
     export PYTHON=/Library/Frameworks/Python.framework/Versions/3.6
     export PYTHONBIN=$PYTHON/bin
@@ -34,7 +32,7 @@ elif [[ "$1" = "legacy" ]]; then
     export ARTISAN_LEGACY_BUILD=true
 else
     # standard local builds
-    printf "Standard build"
+    echo "NOTICE: Standard build"
     export PYTHON=/Library/Frameworks/Python.framework/Versions/3.8
 #    export PYTHON=/Users/luther/.pyenv/versions/3.8.2
     export PYTHONBIN=$PYTHON/bin
@@ -61,7 +59,7 @@ export DYLD_FRAMEWORK_PATH=$QT_PATH/lib
 # translations
 $PYTHONBIN/pylupdate5 artisan.pro
 # there is no full Qt installation on Travis, thus don't run  lrelease
-if [ -z $TRAVIS ]; then
+if [ -z $APPVEYOR ]; then
     $QT_SRC_PATH/bin/lrelease -verbose artisan.pro || true
     for f in translations/qtbase_*.ts
     do
