5bbf2872bdbe47077a887f39d8deb30bf09fc96e
Dave <1648748+roundfile@users.noreply.github.com>
2020-11-19 12:04:06 -0800

    fakeroot

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -3,6 +3,7 @@
 set -ex
 sudo apt-get update -y
 sudo apt-get install -y ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5-default
+sudo apt-get install -y fakeroot
 
 # add libs not installed by default on Qt5.15 any longer
 sudo apt-get install -y libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
